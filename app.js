8d160e0f027b06ed8cdbdd0de125496307cff15f
“Lauryn <laurynmills111@gmail.com>
2020-08-25 12:22:23 -0400

    styling the to dos buttons for complete and trash

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function addTodo(event){
     //TRASH BUTTON
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class ="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
 
     //APPEND TO UL LIST
